cda34f048123d96600186b54eaf3ca165ae7c0ff
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-01-23 21:06:36 -0500

    Update Baal.sol
    add ragequitting in eth

--- contracts/org/molochdao/baal/Baal.sol
+++ contracts/org/molochdao/baal/Baal.sol
@@ -3,76 +3,108 @@ pragma solidity 0.8.0;
 
 contract Baal {
     address[] public memberList;
-    uint256 proposalCount;
-
+    uint256 public proposalCount;
+    uint256 public totalVotes;
     mapping(address => Member) public members;
     mapping(uint256 => Proposal) public proposals;
-    
-    event SubmitProposal(address indexed proposer, address indexed target, uint256 proposal, uint256 value, bytes data);
+    event ProcessProposal(uint256 proposal);
+    event Receive(address indexed sender, uint256 value);
+    event SubmitProposal(address indexed proposer, address indexed target, uint256 proposal, uint256 value, bytes data, bool membership);
     event SubmitVote(address indexed member, uint256 proposal, bool approve);
     
     constructor(address[] memory summoners, uint256[] memory votes) {
         for (uint256 i = 0; i < summoners.length; i++) {
-             members[summoners[i]].votes;
+             totalVotes += votes[i];
+             members[summoners[i]].votes = votes[i];
+             members[summoners[i]].active = true;
              memberList.push(summoners[i]);
         }
     }
     
     struct Member {
         uint256 votes;
-        uint256 lastVote;
+        bool active;
+        mapping(uint256 => bool) voted;
     }
     
     struct Proposal {
-        address proposer;
         address target;
         uint256 value;
         uint256 noVotes;
         uint256 yesVotes;
         bytes data;
         bool membership;
-        bool passed;
+        bool removal;
         bool processed;
     }
     
-    function submitProposal(address target, uint256 value, bytes calldata data, bool membership) external returns (uint256) {
+    receive() external payable {emit Receive(msg.sender, msg.value);}
+    
+    function getMembers() external view returns (address[] memory membership) {
+        return memberList;
+    }
+    
+    function getMemberVote(address member, uint256 proposal) external view returns (bool approved) {
+        return members[member].voted[proposal];
+    }
+    
+    function submitProposal(address target, uint256 value, bytes calldata data, bool membership, bool removal) external returns (uint256 count) {
         proposalCount++;
         uint256 proposal = proposalCount;
-        
-        proposals[proposal] = Proposal(msg.sender, target, value, 0, 0, data, false, false, false);
-        
-        emit SubmitProposal(msg.sender, target, proposal, value, data);
+        proposals[proposal] = Proposal(target, value, 0, 0, data, membership, removal, false);
+        emit SubmitProposal(msg.sender, target, proposal, value, data, membership);
         return proposal;
     }
     
-    function submitVote(uint256 proposal, bool approve) external {
-        Member storage member = members[msg.sender];
+    function submitVote(uint256 proposal, bool approve) external returns (uint256 count) {
+        Member storage member = members[msg.sender]; 
         Proposal storage prop = proposals[proposal];
-        require(member.lastVote != proposal);
-        
+        require(proposal <= proposalCount, "!exist");
+        require(!prop.processed, "processed");
+        require(member.active, "!active");
+        require(!member.voted[proposal], "voted");
         if (approve) {prop.yesVotes += member.votes;} 
         else {prop.noVotes += member.votes;}
-        
-        member.lastVote = proposal;
-        
+        member.voted[proposal] = true;
         emit SubmitVote(msg.sender, proposal, approve);
+        return proposal;
     }
     
-    function processProposal(uint256 proposal) external returns (bool, bytes memory) {
+    function processProposal(uint256 proposal) external returns (bool success, bytes memory retData) {
         Proposal storage prop = proposals[proposal];
-        require(!prop.processed);
-        
+        require(proposal <= proposalCount, "!exist");
+        require(!prop.processed, "processed");
+        prop.processed = true;
+        emit ProcessProposal(proposal);
         if (prop.yesVotes > prop.noVotes) {
-            prop.passed = true;
-            if (prop.membership) {
-                members[prop.proposer].votes += prop.value;
-                memberList.push(prop.proposer);
+            if (prop.membership && !prop.removal) {
+                totalVotes += prop.value;
+                members[prop.target].votes += prop.value;
+                members[prop.target].active = true;
+                memberList.push(prop.target);
+            } else if (prop.membership && prop.removal) {
+                address removedMember = prop.target;
+                uint256 removedVotes = members[removedMember].votes;
+                (bool callSuccess, ) = removedMember.call{value: address(this).balance * removedVotes / totalVotes}("");
+                require(callSuccess, "!ethCall");
+                totalVotes -= removedVotes;
+                members[removedMember].votes = 0;
+                members[removedMember].active = false; 
             } else {
-                (bool success, bytes memory returnData) = prop.target.call{value: prop.value}(prop.data);
-                return (success, returnData);   
+                (bool callSuccess, bytes memory returnData) = prop.target.call{value: prop.value}(prop.data);
+                return (callSuccess, returnData);   
             }
         }
-
-        prop.processed = true;
+    }
+    
+    function rageQuit(uint256 votes) external {
+        require(members[msg.sender].votes >= votes, "!claim");
+        (bool success, ) = msg.sender.call{value: address(this).balance * votes / totalVotes}("");
+        require(success, "!ethCall");
+        totalVotes -= votes;
+        members[msg.sender].votes -= votes;
+        if (members[msg.sender].votes == 0) {
+            members[msg.sender].active = false;
+        }
     }
 }
