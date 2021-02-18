78513f8b93eb83faeca0253027594ec1c18d9203
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-02-18 15:10:57 -0500

    Update SimpleTokenFactory.sol

--- contracts/token/SimpleTokenFactory.sol
+++ contracts/token/SimpleTokenFactory.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract SimpleToken {
-    string constant public name = "Token Asset";
-    string constant public symbol = "ASSET";
+    string public name;
+    string public symbol;
     uint8 constant public decimals = 18;
     uint256 constant public totalSupply = 100 ether;
     
@@ -13,7 +13,9 @@ contract SimpleToken {
     event Transfer(address indexed from, address indexed to, uint256 amount);
     event Approval(address indexed owner, address indexed spender, uint256 amount);
     
-    constructor(address creator) {
+    constructor(address creator, string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
         balanceOf[creator] = 100 ether;
         emit Transfer(address(0), creator, 100 ether);
     }
@@ -41,7 +43,44 @@ contract SimpleToken {
 }
 
 contract SimpleTokenFactory {
+    mapping(address => uint256) private counter;
+    
     fallback() external {
-        new SimpleToken(msg.sender);
+        string memory count = toString(abi.encodePacked(counter[msg.sender]++));
+        new SimpleToken(msg.sender, toString(abi.encodePacked(msg.sender)), concat("ASSET", count));
+        counter[msg.sender]++;
+    }
+    
+    function toString(bytes memory data) private pure returns (string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory str = new bytes(2 + data.length * 2);
+        str[0] = "0";
+        str[1] = "x";
+        for (uint256 i = 0; i < data.length; i++) {
+            str[2+i*2] = alphabet[uint256(uint8(data[i] >> 4))];
+            str[3+i*2] = alphabet[uint256(uint8(data[i] & 0x0f))];
+        }
+        return string(str);
+    }
+    
+    function concat(string memory _base, string memory _value) private pure returns (string memory) {
+        bytes memory _baseBytes = bytes(_base);
+        bytes memory _valueBytes = bytes(_value);
+
+        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
+        bytes memory _newValue = bytes(_tmpValue);
+
+        uint i;
+        uint j;
+
+        for(i=0; i<_baseBytes.length; i++) {
+            _newValue[j++] = _baseBytes[i];
+        }
+
+        for(i=0; i<_valueBytes.length; i++) {
+            _newValue[j++] = _valueBytes[i++];
+        }
+
+        return string(_newValue);
     }
 }
